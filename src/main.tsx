b379f9834f0c288ca8dc1956fc35ab03c6c8b689
VKTiwari3 <ganeshtiwaritiwari05@gmail.com>
2026-05-27 11:50:37 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,13 @@
 src/index.css
 public/favicon.svg
 public/robots.txt
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
